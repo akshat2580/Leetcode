310f66aad510ea37f57281d19881de98924b4aa9
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-24 04:16:24 +0530

    Time: 4 ms (91.13%), Space: 10.8 MB (21.04%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    vector<int> minSubsequence(vector<int>& nums) {
-    vector<int> res;
-    auto sub_sum = 0, half_sum = accumulate(begin(nums), end(nums), 0) / 2;
-    priority_queue<int> pq(begin(nums), end(nums));
-    while (sub_sum <= half_sum) {
-        res.push_back(pq.top());
-        sub_sum += res.back();
-        pq.pop();
-    }
-    return res;
+   vector<int> minSubsequence(vector<int>& nums) {
+	vector<int> res, freq(101, 0);
+	int sumres=0, sumleft=accumulate(begin(nums), end(nums), 0);
+
+	for(auto n: nums)
+		freq[n]++;
+
+	for(int n=100; n>=1; n--){
+		while(freq[n]-->0 && sumres<=sumleft){
+			sumres+=n;
+			sumleft-=n;
+			res.push_back(n);
+		}
+	}
+	return res;
 
     }
 };
