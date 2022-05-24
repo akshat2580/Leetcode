6afc2a8b62b827539cbc1f7573477ec634c85b80
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-25 04:45:01 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (98.55%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-  
-void p(vector<vector<int>>&a,vector<int> n ,int s){
-        if(s==0){
-            a.push_back(n);
-        }
-      else{
-          for(int i=s;i>=0;i--){
-              swap(n[i],n[s]);
-              p(a,n,s-1);
-          }
-         
-      } 
+    vector<vector<int> > permute(vector<int> &num) {
+	    vector<vector<int> > result;
+	    
+	    permuteRecursive(num, 0, result);
+	    return result;
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-         vector<vector<int>>v;
-       p(v,nums,nums.size()-1); 
-        return v;
+    
+    // permute num[begin..end]
+    // invariant: num[0..begin-1] have been fixed/permuted
+	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+		if (begin >= num.size()) {
+		    // one permutation instance
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    // reset
+		    swap(num[begin], num[i]);
+		}
     }
 };
