6b907921520ba4b9d2c748dd380faf19c6173d79
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-24 10:17:25 +0530

    Time: 29 ms (59.88%), Space: 18 MB (90.28%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,34 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValidSudoku(vector<vector<char>>& a) {int c=0,k=0,l=0;
-       for(int i=0;i<9;i++){map<char,int>m;
-                            map<char,int>n;
-           for(int j=0;j<9;j++){
-               if(a[i][j]!='.')
-               m[a[i][j]]++;
-               if(a[j][i]!='.')
-               n[a[j][i]]++;
-           }
-           
-        for(auto i:m)if(i.second>1)c++;
-        for(auto i:n)if(i.second>1)c++;
-       } 
-      while(k<9){ 
-          l=0;
-         while(l<9){
-             map<char,int>m;
-        for(int i=k;i<3+k;i++){
-             for(int j=l;j<3+l;j++){
-                if(a[i][j]!='.')
-                 m[a[i][j]]++;
-             }        
-         }
-          for(auto i:m)if(i.second>1)c++;
-                         
-              l+=3;}
-                 k+=3;                                   
-         }                                        
-       if(c>0)return false;
-    else   return true;
+    bool isValidSudoku(vector<vector<char> > &board)
+    {
+        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+        
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return false;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                }
+        
+        return true;
     }
 };
