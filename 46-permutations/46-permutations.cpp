98523bc33b4abe505de1775ddc6d10c5b74f51a2
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-25 04:43:01 +0530

    Time: 8 ms (26.17%), Space: 8 MB (28.52%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    
+  
 void p(vector<vector<int>>&a,vector<int> n ,int s){
-        if(s==n.size()){
+        if(s==0){
             a.push_back(n);
         }
       else{
-          for(int i=s;i<n.size();i++){
+          for(int i=s;i>=0;i--){
               swap(n[i],n[s]);
-              p(a,n,s+1);
+              p(a,n,s-1);
           }
          
       } 
     }
     vector<vector<int>> permute(vector<int>& nums) {
-       vector<vector<int>>v;
-       p(v,nums,0); 
+         vector<vector<int>>v;
+       p(v,nums,nums.size()-1); 
         return v;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> perms;
+//         permute(nums, 0, perms);
+//         return perms;
+//     }
+// private:
+//     void permute(vector<int> nums, int i, vector<vector<int>>& perms) {
+//         if (i == nums.size()) {
+//             perms.push_back(nums);
+//         } else {
+//             for (int j = i; j < nums.size(); j++) {
+//                 swap(nums[i], nums[j]);
+//                 permute(nums, i + 1, perms);
+//             }
+//         }
+//     }
+// };
