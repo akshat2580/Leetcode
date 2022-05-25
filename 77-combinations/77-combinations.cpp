153a414d33eac5aed497ba3fccd1c0e931367d51
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-26 02:12:29 +0530

    Time: 32 ms (62.29%), Space: 8.8 MB (97.63%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    void p(vector<vector<int>>&v,int i,int n,int k,vector<int>&a){
-      if(a.size()==k){
-          v.push_back(a);
-          return;
-    }
-    if(i>n)return;
-        a.push_back(i);
-        p(v,i+1,n,k,a);
-        a.pop_back();
-         p(v,i+1,n,k,a);
-        a.push_back(i);
-        a.pop_back();
-    
-}
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>v;vector<int>a;
-        p(v,1,n,k,a);
-        return v;
-    }
+	vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
 };
