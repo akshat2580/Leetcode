57355977779c894186313de59ae01f0496c4d7d1
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-27 23:56:35 +0530

    Time: 2 ms (40.77%), Space: 5.9 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-    int numberOfSteps(int num) { // number of steps to reach 0
-        if(num==0) return 0; // base case 1 
-        if(num==1) return 1; // base case 2
-        return numberOfSteps(num/2)+(num%2==0?1:2); // recursive case and add 1 if num is odd else add 2
+int numberOfSteps(int num) { //  number of steps to reach 0    
+        int count = 0; // number of steps 
+        while(num){ // while n is not 0
+            num = num & 1 ? num - 1 : num >> 1; // if n is odd, subtract 1, else right shift by 1
+            count++; // increment count
+        }
+        return count; // return count
     }
 };
