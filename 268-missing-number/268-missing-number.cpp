9d7500f7202a850508c2b3c5731bc8a8b25429cc
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-05-28 13:38:48 +0530

    Time: 16 ms (92.56%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,9 +3,9 @@ public:
     int missingNumber(vector<int>& v) {long long c=0,r=0;
         int n=v.size();long long s=(n*(n+1))/2;
         for(int i=0;i<n;i++){
-           c+=v[i];
+           c+=v[i]-i;
         }
        
-        return s-c;
+        return n-c;
     }
 };
