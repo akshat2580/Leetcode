8bd7a48680c6501560f121834e6aa84dcb0243eb
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-02 05:32:20 +0530

    Time: 1382 ms (15.58%), Space: 9.1 MB (78.25%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,29 +1,18 @@
 class Solution {
- public:
-  
-    int findTargetSumWays(vector<int>& s, int t) {
-        int n=s.size(),l=0;
-        if(s.size()==1)return abs(s[0])==abs(t);
+ public:int c=0;
+    void solve(vector<int>&s,int n,int t){
+        if(n==s.size()){
+            if(t==0)
+            c++;
+        }
+        else{
+      solve(s,n+1,t+s[n]);
+        solve(s,n+1,t-s[n]);
+        }
         
-                 for(auto i:s)l+=i; 
-       
-              int k=(l+t)/2;
-        if((l+t)%2||abs(l)<abs(t))return 0;
-        int dp[n+1][k+1];
-        for(int i=0;i<=n;i++)dp[i][0]=1;
-        for(int j=1;j<=k;j++)dp[0][j]=0;
-          for(int i=1;i<=n;i++){
-              for(int j=0;j<=k;j++){
-             
-              
-                   if(s[i-1]<=j)
-                  {
-                      dp[i][j]=dp[i-1][j]+dp[i-1][j-s[i-1]];
-                  }
-                  else
-                      dp[i][j]=dp[i-1][j];
-              }
-          } 
-        return dp[n][k];
+    }
+    int findTargetSumWays(vector<int>& s, int t) {
+      solve(s,0,t);
+        return c;
     }
 };
