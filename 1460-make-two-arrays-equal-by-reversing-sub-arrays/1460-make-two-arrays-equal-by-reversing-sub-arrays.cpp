83523d2c0220d0bb088f75b4ab9e2f790cfb04c9
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-04 07:47:52 +0530

    Time: 31 ms (13.25%), Space: 18 MB (6.32%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& t, vector<int>& a) {
-        sort(t.begin(),t.end());
-        sort(a.begin(),a.end());
-        return a==t;
+        multiset<int>s1(t.begin(),t.end()),s2(a.begin(),a.end());
+        return s1==s2;
     }
 };
