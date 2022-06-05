45f3f83f1917c85789f9202daec783499e419d6c
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-05 18:26:14 +0530

    Time: 6 ms (64.32%), Space: 6.6 MB (38.33%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,21 +1,33 @@
-
-  class Solution {
+class Solution {
 public:
-    int totalNQueens(int N) {
-        ans = 0;
-        place(0,0,0,0,N);
-        return ans;
+    vector<vector<string>>v;
+    bool c(vector<string>&s,int r,int m){
+        for(int i=r;i>=0;i--)
+            if(s[i][m]=='Q')return false;
+        for(int i=r,j=m;i>=0&&j>=0;--i,--j)
+            if(s[i][j]=='Q')return false;
+        for(int i=r,j=m;j<s.size()&&i>=0;--i,++j)
+            if(s[i][j]=='Q')return false;
+        return true;
+        
     }
     
-private:
-    int ans;
-    
-    void place(int i, int vert, int ldiag, int rdiag, int N) {
-        if (i == N) ans++;
-        else for (int j = 0; j < N; j++) {
-            int vmask = 1 << j, lmask = 1 << (i+j), rmask = 1 << (N-i-1+j);
-            if (vert & vmask || ldiag & lmask || rdiag & rmask) continue;
-            place(i+1, vert | vmask, ldiag | lmask, rdiag | rmask, N);
+    void d(vector<string>&s,int n){
+        if(n==s.size()){
+            v.push_back(s);
+            return;
         }
+        for(int i=0;i<s.size();++i)
+            if(c(s,n,i)){
+                s[n][i]='Q';
+                d(s,n+1);
+                s[n][i]='.';
+            }
+    }    
+    int totalNQueens(int n) {
+        
+           vector<string>s(n,string(n,'.'));
+        d(s,0);
+        return v.size();
     }
-};  
+};
