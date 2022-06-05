79a61e643ff91c00f9924e4fb7721267f8cb18be
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-06 04:11:35 +0530

    Time: 117 ms (60.80%), Space: 36.9 MB (60.09%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 0; i < list1.size(); i++) m[list1[i]] = i;
         for(int i = 0; i < list2.size(); i++)
             if(m.count(list2[i]) != 0)
-                if(m[list2[i]] + i < min) min = m[list2[i]] + i, res.clear(), res.push_back(list2[i]);
+                if(m[list2[i]] + i < min) {min = m[list2[i]] + i; res.clear(); res.push_back(list2[i]);}
                 else if(m[list2[i]] + i == min) res.push_back(list2[i]);
         return res;
     }
