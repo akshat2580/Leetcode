3d0615d60406796f6d883a863d73e803ae0d09bb
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-08 05:11:30 +0530

    Time: 5 ms (27.60%), Space: 6.5 MB (68.19%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void dfs(vector<int>&a,vector<vector<int>>&v,int k,int n,int i){
+if(n==0&&k==a.size()){
+    v.push_back(a);
+    return ;
+}
+    if(n<0)return;
+        for(int j=i;j<=9;j++){
+            a.push_back(j);
+            dfs(a,v,k,n-j,j+1);
+            a.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>v;
+        vector<int>a;
+        dfs(a,v,k,n,1);
+        return v;
+    }
+};
