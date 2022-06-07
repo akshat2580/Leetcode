8ad7387f28579680276cbc838ba33e8fcbbbadec
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-07 23:39:08 +0530

    Time: 57 ms (29.84%), Space: 22.1 MB (26.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
-public:
-    std::vector<std::vector<int> > combinationSum(std::vector<int> &candidates, int target) {
-        std::sort(candidates.begin(), candidates.end());
-        std::vector<std::vector<int> > res;
-        std::vector<int> combination;
-        combinationSum(candidates, target, res, combination, 0);
-        return res;
-    }
-private:
-    void combinationSum(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
-        if (!target) {
-            res.push_back(combination);
+public:  vector<vector<int>>v;
+    void s(vector<int>a,vector<int>&c,int t,int i){
+        if(t<0)return;
+        if(t==0)
+        {v.push_back(a);
             return;
         }
-        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
-            combination.push_back(candidates[i]);
-            combinationSum(candidates, target - candidates[i], res, combination, i);
-            combination.pop_back();
+      for(int j=i;j<c.size();j++)
+        {a.push_back(c[j]);
+            s(a,c,t-c[j],j);
+         a.pop_back();
         }
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        vector<int>a;
+        s(a,c,t,0);
+        return v;
     }
 };
