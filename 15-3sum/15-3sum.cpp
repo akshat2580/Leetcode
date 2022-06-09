5e156ff2131d8a5e3f03978fb0453010003c0395
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-10 01:25:24 +0530

    Time: 1429 ms (8.14%), Space: 81 MB (9.96%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -8,23 +8,31 @@ public:
         for(auto i:s)m[i]++;
         for(int i=0 ; i<s.size() ; i++){
             if(i && s[i]==s[i-1])continue;
-            int target=-s[i];
-            int j=i+1,k=s.size()-1;
-            while(j<k){
-                if(s[j]+s[k]>target)k--;
-                else if(s[j]+s[k]<target)j++;
-                else{
-                    v.push_back({s[i],s[j],s[k]});
-                    while(j<k && s[j]==s[j+1])j++;
-                    j++;
-                }
-            }
-            // int a=m[target];
-            // m[target]--;
-            // for(int j=i+1 ; j<s.size() ; j++){
-            //     if(m[target-s[j]]);
+            int target=s[i];
+            // int j=i+1,k=s.size()-1;
+            // while(j<k){
+            //     if(s[j]+s[k]>target)k--;
+            //     else if(s[j]+s[k]<target)j++;
+            //     else{
+            //         v.push_back({s[i],s[j],s[k]});
+            //         while(j<k && s[j]==s[j+1])j++;
+            //         j++;
+            //     }
             // }
+            int a=m[target];
+            m[target]--;
+            for(int j=i+1 ; j<s.size() ; j++){int b=m[s[j]];
+                                              m[s[j]]--;
+                if(m[-target-s[j]])
+                {vector<int>t(3);t={target,s[j],-target-s[j]};
+                 sort(t.begin(),t.end());
+                    v.push_back(t);}
+                   m[s[j]]=b;                           
+            }
+            m[target]=a;
         }
-        return v;
+        set<vector<int>>l(v.begin(),v.end());
+        vector<vector<int>>w(l.begin(),l.end());
+        return w;
     }
 };
