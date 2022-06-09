33780402ef4a5970ef174ba8ac03603667ebc194
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-10 01:16:34 +0530

    Time: 85 ms (86.75%), Space: 20.7 MB (50.31%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,13 +7,16 @@ public:
         unordered_map<int,int>m;
         for(auto i:s)m[i]++;
         for(int i=0 ; i<s.size() ; i++){
+            if(i && s[i]==s[i-1])continue;
             int target=-s[i];
             int j=i+1,k=s.size()-1;
             while(j<k){
                 if(s[j]+s[k]>target)k--;
                 else if(s[j]+s[k]<target)j++;
                 else{
-                    v.push_back({s[i],s[j],s[k--]});
+                    v.push_back({s[i],s[j],s[k]});
+                    while(j<k && s[j]==s[j+1])j++;
+                    j++;
                 }
             }
             // int a=m[target];
@@ -22,8 +25,6 @@ public:
             //     if(m[target-s[j]]);
             // }
         }
-        set<vector<int>> st(v.begin(),v.end());
-        vector<vector<int>> w(st.begin(),st.end());
-        return w;
+        return v;
     }
 };
