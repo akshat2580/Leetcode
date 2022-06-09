b1fe15b37e399a61b4cab87d68bd04a9b700ddc2
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-09 06:44:51 +0530

    Time: 18 ms (58.29%), Space: 15.9 MB (6.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,18 +3,13 @@ public:
     vector<int> twoSum(vector<int>&s, int t) {
         int n=s.size();
          vector<int>v(2);
-        int a=0,b=n-1;
-        while(a<b){
-            if(s[a]+s[b]==t)
-            {
-                v[0]=a+1;
-                v[1]=b+1;
-                break;
-            }
-            else if(t-s[a]<s[b])b--;
-            else a++;
-                
-        }
+       
+        map<int,int>m;int i;
+        for( i=0;i<n;i++)
+            if(m[t-s[i]]){v[1]=i+1;break;}
+                else
+            m[s[i]]=i+1;
+        v[0]=m[t-s[i]];
         return v;
     }
 };
