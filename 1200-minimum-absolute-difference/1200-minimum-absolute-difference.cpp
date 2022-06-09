fc68e12f579c75c9d291e18920e1587ec5543b56
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-10 02:16:46 +0530

    Time: 85 ms (70.76%), Space: 32.1 MB (80.76%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,14 +4,13 @@ public:
         vector<vector<int>>v;
         int n=arr.size();
         sort(arr.begin(),arr.end());
-        unordered_map<int,int>q;
-        for(auto i:arr)q[i]++;
+       
         int m=INT_MAX;
         for(int i=1;i<n;i++)
         m=min(m,abs(arr[i]-arr[i-1]));
-        for(int i=0;i<n;i++){
-            if(q[arr[i]-m])
-                v.push_back({min(arr[i],arr[i]-m),max(arr[i],arr[i]-m)});
+        for(int i=0;i<n-1;i++){
+            if(-arr[i]+arr[i+1]==m)
+                v.push_back({arr[i],arr[i+1]});
         }
         return v;
     }
