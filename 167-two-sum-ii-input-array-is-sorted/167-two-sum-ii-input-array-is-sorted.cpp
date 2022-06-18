2431c9403412534f23ca2e29eddfa91b16d03218
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-18 17:01:24 +0530

    Time: 29 ms (15.71%), Space: 16.1 MB (5.73%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,16 @@
 class Solution {
-public: 
-    vector<int> twoSum(vector<int>&s, int t) {
-        int l=0,r=s.size()-1;
-       while(l<r){
-           if(s[l]+s[r]==t)return {l+1,r+1};
-           else if(s[l]+s[r]>t)r--;
-           else l++;
-       }
-        return {};
+public:
+    vector<int> twoSum(vector<int>& s, int t) {
+        unordered_map<int,int>m;
+        int n=s.size();
+        vector<int>v(2,-1);
+        for(int i=0;i<n;i++)m[s[i]]=i+1;
+        for(int i=0;i<n;i++){
+            if(m[t-s[i]]&&m[t-s[i]]!=i+1){
+                v[1]=m[t-s[i]];v[0]=i+1;
+            }
+        }
+        sort(v.begin(),v.end());
+        return v;
     }
 };
