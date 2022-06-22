66469980ab3963c619b164d3146aed8302aeb40a
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-22 18:12:29 +0530

    Time: 292 ms (77.63%), Space: 55.3 MB (79.50%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,14 +1,19 @@
+class NumStrMaxComparator {
+public:
+    bool operator() (string &a, string &b) {
+        if (a.size() != b.size()) return a.size() < b.size();
+        return  a < b;
+    }
+};
+
 class Solution {
 public:
-    struct comp {
-        bool operator()(const std::string& a, const std::string& b) const {
-            if (a.size() != b.size()) return a.size() < b.size();
-            return a < b;
-        }
-    };
-    
     string kthLargestNumber(vector<string>& nums, int k) {
-        nth_element(nums.begin(), nums.end()-k, nums.end(), comp());
-        return nums[nums.size()-k];
+        make_heap(nums.begin(), nums.end(), NumStrMaxComparator()); // heapify cost O(N)
+        while (k-- > 1) {
+            pop_heap(nums.begin(), nums.end(), NumStrMaxComparator());
+            nums.pop_back();
+        }
+        return nums.front();
     }
 };
