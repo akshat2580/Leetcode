cbc07be4b6151bd0302b1f48687964c5346acaef
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-27 16:01:52 +0530

    Time: 16 ms (38.95%), Space: 11.1 MB (5.33%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int>m;
+        vector<int>v; 
+        for(auto i:nums)m[i]++;
+        for(auto i:m)if(i.second==1 ) v.push_back(i.first) ;
+            return v;
+    }
+};
