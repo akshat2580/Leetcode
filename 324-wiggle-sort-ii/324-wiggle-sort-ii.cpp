c54fbd2f8642d43ee0099ba6d75868fe1f26172a
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-28 03:05:53 +0530

    Time: 19 ms (93.62%), Space: 17.8 MB (53.66%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -3,7 +3,12 @@ public:
    void wiggleSort(vector<int>& s) {
     vector<int> v(s);
     sort(v.begin(), v.end());
-    for (int i=s.size()-1, j=0, k=i/2+1; i>=0; i--)
-        s[i] = v[i%2 ? k++ : j++];
+       int j=0, k=(s.size()-1)/2+1;
+    for (int i=s.size()-1; i>=0; i--)
+        if(i%2)
+        s[i]=v[k++];
+       else
+            s[i]= v[j++];
+           
 }
 };
