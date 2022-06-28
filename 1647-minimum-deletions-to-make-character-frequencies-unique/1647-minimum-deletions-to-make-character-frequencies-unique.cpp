9783aaf7cb0f1fb812bc6588bc4b7a592dbd2c8a
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-28 15:39:49 +0530

    Time: 82 ms (59.59%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -5,7 +5,8 @@ public:
     unordered_set<int> a;
     for (char i : s)c[i-'a']++;
     for (int i = 0; i < 26; i++)
-        while(c[i] > 0 && !a.insert(c[i]).second)
+        while(c[i] > 0 && !a.insert(c[i]).second)//here, second will give the bool value of the insert function, insert function has actually has 2 value stored if a elemnt is stored in bool it is written as true; hence if the is another a value comes then bool will get true,meaning that it already has a value.
+            
         {r++;; c[i]--;}
     return r;
 }
