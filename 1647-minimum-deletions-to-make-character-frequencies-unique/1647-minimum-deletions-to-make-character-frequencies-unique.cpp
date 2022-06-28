43424018dae4d9fd02e769a9d27f256e04779508
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-06-28 15:24:43 +0530

    Time: 45 ms (98.67%), Space: 17.4 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -4,7 +4,7 @@ public:
     int c[26] = {}, r = 0;
     unordered_set<int> a;
     for (char i : s)c[i-'a']++;
-    for (int i = 0; i < 26; ++i)
+    for (int i = 0; i < 26; i++)
         while(c[i] > 0 && !a.insert(c[i]).second)
         {r++;; c[i]--;}
     return r;
