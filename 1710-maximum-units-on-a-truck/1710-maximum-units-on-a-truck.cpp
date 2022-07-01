7b689544be1cc5fb4c0a5f49bcb5923f5ffd5bcf
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-01 14:22:07 +0530

    Time: 51 ms (81.25%), Space: 15.9 MB (95.14%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int maximumUnits(vector<vector<int>>& B, int T) {
-        sort(B.begin(), B.end(), [](auto a,auto b) { return b[1] < a[1];});
-        int ans = 0;
-        for (auto& b : B) {
-            int count = min(b[0], T);
-            ans += count * b[1], T -= count;
-			if (!T) return ans;
+    int maximumUnits(vector<vector<int>>& b, int t) {
+        int c=0,n=b.size();
+        sort(b.begin(),b.end(),[](auto& a,auto& b){
+            return a[1]>b[1];
+        });
+             int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=min(b[i][0],t);
+            ans+=x*b[i][1];
+            t-=x;
+            if(!t)break;
         }
         return ans;
     }
 };
+
+       
