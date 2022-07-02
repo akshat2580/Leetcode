bc680e0b9abaf1c8a8590f520b8bc5394911080c
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-03 02:33:58 +0530

    Time: 3 ms (52.05%), Space: 6.2 MB (32.23%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/492-construct-the-rectangle.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    vector<int> constructRectangle(int a) {
-        int l=0,r=0,d=INT_MAX;
-        for(int i=1;i<=a;i++)
-            if(d>(i-a/i)&&i>=a/i&& i*(a/i)==a){
-               l=i;r=a/i;d=(i-a/i);
-            }
-            return {l,r};
+   vector<int> constructRectangle(int area) {
+        for(int mid = sqrt(area); mid>0; mid--)
+            if (!(area%mid))
+                return {area/mid, mid};
+       return {};
     }
 };
