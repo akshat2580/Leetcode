0a1f480c1fcd75e85fca6111ec8c0fe8fd7a5614
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-03 09:46:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (98.43%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-
     vector<vector<int>> generateMatrix(int n) {
-
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
+        int r1 = 0, r2 = n-1;
+        int c1 = 0, c2 = n-1;
+        int val = 0;
+		
+	
+        vector<vector<int>> v(n, vector<int> (n));
+        while(r1 <= r2 && c1 <= c2)
+        {
+            
+            for(int i = c1; i <= c2; ++i)
+                v[r1][i] = ++val;
+				
+				
+            for(int i = r1+1; i <= r2; ++i)
+                v[i][c2] = ++val;
+				
+         
+            if(r1 < r2 && c1 < c2)
+            {
+                
+                for(int i = c2-1; i>c1; --i)
+                    v[r2][i] = ++val;
+					
+				
+					for(int i = r2; i>r1; --i) 
+                    v[i][c1] = ++val;
             }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
-            }
-            // direction 3 - traverse from right to left
-            for (int ptr = n - layer - 2; ptr >= layer; ptr--) {
-                result[n - layer - 1][ptr] = cnt++;
-            }
-            // direction 4 - traverse from bottom to top
-            for (int ptr = n - layer - 2; ptr > layer; ptr--) {
-                result[ptr][layer] = cnt++;
-            }
+            ++r1;
+            --r2;
+            ++c1;
+            --c2;
         }
-
-        return result;
+        return v;
     }
 };
