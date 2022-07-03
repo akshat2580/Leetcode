2d97cbfc8e2c1f0b12bc4872d808bf0059fac633
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-03 16:35:18 +0530

    Time: 18 ms (5.90%), Space: 9.1 MB (81.67%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int countElements(vector<int>& s) {
-        int n=s.size();
-    map<int,int>m;for(auto i:s)m[i]++;
-        nth_element(s.begin(),s.begin()+n-1,s.end());
-        nth_element(s.begin(),s.begin(),s.end());
-      
-       if(s[0]==s[n-1])return 0;
-        return n-m[s[0]]-m[s[n-1]];
+    int countElements(vector<int>& nums) {
+        int M = *max_element(nums.begin(), nums.end()); 
+        int m = *min_element(nums.begin(), nums.end()); 
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > m && nums[i] < M) res++;
+        }
+        return res;
     }
 };
