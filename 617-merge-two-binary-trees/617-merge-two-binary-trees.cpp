65e6ab882870fa35e01072c5c8074fae69b8b6e8
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-10 02:30:10 +0530

    Time: 45 ms (82.51%), Space: 33.6 MB (15.60%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,14 +11,28 @@
  */
 class Solution {
 public:
+    void dfs(TreeNode* t1, TreeNode* t2,TreeNode* &root){
+        if(!t1 && !t2) return;
+        else if(t1 && !t2){
+            TreeNode* node(new TreeNode(t1->val));
+            root=node;
+            dfs(t1->left,t2,root->left);
+            dfs(t1->right,t2,root->right);
+        }else if(t2 && !t1){
+            TreeNode* node(new TreeNode(t2->val));
+            root=node;
+            dfs(t1,t2->left,root->left);
+            dfs(t1,t2->right,root->right);
+        }else{
+            TreeNode* node(new TreeNode(t1->val+t2->val));
+            root=node;
+            dfs(t1->left,t2->left,root->left);
+            dfs(t1->right,t2->right,root->right);
+        }
+    }
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if(!t1 && !t2) return nullptr;
-        int val1 = t1? t1->val:0;
-        int val2 = t2? t2->val:0;
-        
-        TreeNode* t = new TreeNode(val1+val2);
-        t->left = mergeTrees(t1?t1->left:nullptr,t2?t2->left:nullptr);
-        t->right = mergeTrees(t1?t1->right:nullptr,t2?t2->right:nullptr);
-        return t;
+        TreeNode* root(nullptr);
+        dfs(t1,t2,root);
+        return root;
     }
 };
