a1f96371a712df0e4333d09cf52a82959304e032
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-11 03:59:34 +0530

    Time: 3 ms (50.85%), Space: 6.6 MB (22.76%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int countPoints(string s) {int c=0;
-        map<char,set<char>>m;
-         for(int i=0;i<s.size();i++){
-             if(i%2)m[s[i]].insert(s[i-1]);
-         }
-        for(auto i:m){
-            if(i.second.size()==3)c++;
-        }
-                               return c;
+    int countPoints(string r) {
+        unordered_map<char,set<char>>m;
+        int c=0;
+        for(auto i=0;i<r.size();i-=-1)
+            if(r[i]!='B'&&r[i]!='G'&&r[i]!='R')
+                m[r[i]].insert(r[i-1]);
+                for(auto i:m)if(i.second.size()==3)c++;
+        return c;
     }
 };
