6458f4b02e9ea75b7a11a901f066a580092bdf93
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-11 17:44:36 +0530

    Time: 7 ms (48.55%), Space: 7.8 MB (23.47%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int sol(vector<int>&v,int x,int y,int i ){
-         if (x==y)
-            return i*v[x];
-        int a = i*v[x] + sol(v,x+1,y,-i);
-        int b = i*v[y] + sol(v,x,y-1,-i);
-        return i* max(i*a,i*b); 
-        
-    }
-    
     bool PredictTheWinner(vector<int>& nums) {
-        return sol(nums,0,nums.size()- 1,1)>=0;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n)); // use to keep the score gap between player1 and player2
+        for (int i = 0; i < n; i++) dp[i][i] = nums[i];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j+i < n; j++) {
+                dp[j][j+i] = max(nums[j+i]-dp[j][j+i-1], nums[j]-dp[j+1][j+i]);
+            }
+        }
+        return dp[0][n-1] >= 0; // player1 get more score points than player2
     }
 };
