cb0de263f43db52b8c496aee398daf266201db01
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-13 02:52:17 +0530

    Time: 34 ms (17.35%), Space: 23.6 MB (5.47%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -11,22 +11,18 @@
  */
 class Solution {
 public:
+    void dfs(TreeNode* root, int layer, vector<vector<int>> &tmp) {
+        if (tmp.size() < layer + 1) tmp.push_back({});
+        tmp[layer].push_back(root->val);
+        if (root->left) dfs(root->left, layer + 1, tmp);
+        if (root->right) dfs(root->right, layer + 1, tmp);
+    }
     vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*>q;
-        vector<double>v;
-        if(root==nullptr)return v;
-        q.push(root);
-        while(q.size()){
-            int n=q.size();double s=0;int c=0;
-            while(n--){TreeNode* k=q.front();
-                 q.pop();
-                s+=k->val;
-            if(k->right)q.push(k->right);
-                       if(k->left)q.push(k->left);
-                c++;
-            }
-            v.push_back(s/c);
-        }
-        return v;
+        if (!root) return {};
+        vector<vector<int>> tmp(1);
+        dfs(root, 0, tmp);
+        vector<double> res(tmp.size());
+        transform(begin(tmp), end(tmp), begin(res), [](auto v){return accumulate(begin(v), end(v), 0.0) / v.size();});
+        return res;
     }
 };
