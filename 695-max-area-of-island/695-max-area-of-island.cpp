5e50a274a4c0de627f464e17abb6a2105bfeda1d
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-15 15:21:51 +0530

    Time: 14 ms (97.66%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans = 0;
-        n = grid.size(), m = grid[0].size();
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < m; j++)
-                if (grid[i][j]) ans = max(ans, trav(i, j, grid));
-        return ans;
+    void sol(vector<vector<int>>&v,int& a,int i ,int j){
+        if(i==-1||j==-1||j>=v[0].size()||i>=v.size()||v[i][j]==0)
+            return ;
+        a++;
+        v[i][j]=0;
+        sol(v,a,i+1,j);
+        sol(v,a,i,j+1);
+        sol(v,a,i-1,j);
+        sol(v,a,i,j-1);
+            
     }
-private:
-    int n, m;
-    int trav(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0;
-        grid[i][j] = 0;
-        return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
+    int maxAreaOfIsland(vector<vector<int>>& g) {
+        int n=g.size();
+        int m=g[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int a=0;
+                sol(g,a,i,j);
+                ans=max(ans,a);
+            }
+        }
+        return ans;
     }
 };
