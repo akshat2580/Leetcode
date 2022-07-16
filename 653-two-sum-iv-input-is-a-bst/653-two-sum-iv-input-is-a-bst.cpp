abaedcfd0e37147e61ae17d6bb1ed64c666bc37a
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-16 22:42:38 +0530

    Time: 73 ms (36.53%), Space: 38.4 MB (36.16%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,20 +11,19 @@
  */
 class Solution {
 public:
-    vector<int>v;
-    void sol(TreeNode* root){
+    
+    void sol(TreeNode* root,vector<int>& v){
         if(root==nullptr)return;
         v.push_back(root->val);
-        if(root->left)sol(root->left);
-        if(root->right)sol(root->right);
+        if(root->left)sol(root->left,v);
+        if(root->right)sol(root->right,v);
     }
     
     bool findTarget(TreeNode* root, int k) {
-       map<int,int>m;
-        sol(root);
+       unordered_map<int,int>m;
+        vector<int>v;
+        sol(root,v);
         if(v.size()==1)return 0;
-        for(auto i:m)cout<<i.first;
-        cout<<endl;
          for(int i=0;i<v.size();i++)
          { if(m.find(k-v[i])!=m.end())return true;
          m[v[i]]++;
