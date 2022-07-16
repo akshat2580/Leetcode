01523d0226e1007539c45562fface447a398401f
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-16 18:09:38 +0530

    Time: 78 ms (25.84%), Space: 20.8 MB (39.97%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        int n=str.size();
+        map<string,vector<string>>m;
+        for(int i=0;i<n;i++){
+            string g=str[i];
+            sort(g.begin(),g.end());
+            m[g].push_back(str[i]);
+        }
+        vector<vector<string>>v;
+        for(auto i:m)v.push_back(i.second);
+        return v;
+    }
+};
