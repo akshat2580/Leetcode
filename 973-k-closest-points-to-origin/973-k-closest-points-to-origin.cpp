6c1970eead2f9b4f926c4f32dcbd7724c66db379
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-17 05:17:09 +0530

    Time: 710 ms (14.50%), Space: 85.5 MB (13.34%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        partial_sort(points.begin(), points.begin() + K, points.end(), [](vector<int>& p, vector<int>& q) {
-            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
-        });
-        return vector<vector<int>>(points.begin(), points.begin() + K);
+    vector<vector<int>> kClosest(vector<vector<int>>& s, int k) {
+        priority_queue<pair<double,vector<int>>>pq;
+        for(auto i:s){
+            int x=i[0];
+            int y=i[1];
+            pq.push({sqrt(x*x+y*y),{x,y}});
+            if(pq.size()>k)pq.pop();
+        }
+        vector<vector<int>>v;
+        while(pq.size()){
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+        return v;
     }
 };
