48e3192ea671396d51ed5610418ed0ff2bdcbf0f
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-17 05:37:31 +0530

    Time: 226 ms (59.97%), Space: 59.4 MB (74.03%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int nearestValidPoint(int x, int y, vector<vector<int>>& s) {
-       map<int,int>m;int n=s.size();int c=INT_MAX;
-        for(int i=0;i<n;i++){
-            if((s[i][0]==x||s[i][1]==y))
-            {            if(m.find((abs(x-s[i][0])+abs(y-s[i][1])))==m.end())
-                m[(abs(x-s[i][0])+abs(y-s[i][1]))]=i;
-            c=min(c,(abs(x-s[i][0])+abs(y-s[i][1])));
-            }
-        }
-        if(m.find(c)==m.end())return -1;
-        return m[c];
+  int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+	int n = points.size(), mn = INT_MAX, ans = -1, manhattan;
+	for(int i = 0; i < n; i++)
+		if(points[i][0] == x || points[i][1] == y){
+			manhattan = abs(x - points[i][0]) + abs(y - points[i][1]);
+			if(manhattan < mn)
+				mn = manhattan, ans = i;            
+		}
+	return ans;
+
     }
 };
