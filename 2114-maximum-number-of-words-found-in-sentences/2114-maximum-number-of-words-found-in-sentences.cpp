c3e1a30bc053eb8fafcc35c5f9919c3a0650a4ef
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-24 02:45:58 +0530

    Time: 13 ms (83.36%), Space: 10.4 MB (7.84%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -3,8 +3,8 @@ public:
     int mostWordsFound(vector<string>& s) {
         int c=0;
         for(auto i:s){
-            string g=i;int v=1;
-            for(auto j:g)if(j==' ')v++;
+            int v=1;
+            for(auto j:i)if(j==' ')v++;
             c=max(c,v);
         }
         return c;
