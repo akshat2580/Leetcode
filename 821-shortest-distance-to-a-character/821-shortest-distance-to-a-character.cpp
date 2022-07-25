3354ef02ad8e07bc3e55253cb29aa7a9dd9784e2
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-25 11:56:46 +0530

    Time: 6 ms (41.69%), Space: 6.8 MB (16.46%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
-         vector<int>v;
-        vector<int>m;
-        for(int i=0;i<s.size();i++)
-            if(s[i]==c)
-            m.push_back(i);
-        for(int i=0;i<s.size();i++){int k=INT_MAX;
-            for(auto j:m){
-                k=min(abs(i-j),k);
-            }v.push_back(k);
+        vector<int> ioc; // vector for storing the indexed of c present in s
+        int n=s.length();
+        
+        vector<int>ans(n); // answer vector
+        
+        for(int i=0;i<n;++i){
+            if(s[i]==c) 
+                ioc.push_back(i);
         }
-        return v;
+        
+        int m=ioc.size(); // size of ioc vector
+        int left=0,right=0;
+        
+        for(int i=0;i<n;++i){
+            
+          
+            if(i>ioc[right]){
+                left=right;
+                if(right<m-1)
+                    ++right;
+            }
+                        
+            ans[i]=min(abs(ioc[right]-i),abs(ioc[left]-i)); 
+
+        }
+        
+        return ans;
     }
 };
