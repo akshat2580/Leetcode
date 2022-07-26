91c719ea586dd0188e1c474da80ebb9d0a40881f
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-07-26 11:51:37 +0530

    Time: 695 ms (71.74%), Space: 82.1 MB (96.94%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int maximumProduct(vector<int>& s, int k) {
-        priority_queue<int,vector<int>,greater<int>>q;
-        for(auto i:s)q.push(i);
-        while(k--){
-            int j=q.top()+1;q.pop();
-            q.push(j);
-        }
-        long long c=1;
-        while(q.size()){c=c%(1000000000+7);
-            c*=q.top();q.pop();
-        }
-        c=c%(1000000000+7);
-        return (int)c;
+   int maximumProduct(vector<int>& nums, int k) {
+    make_heap(begin(nums), end(nums), greater<int>());
+    while (k--) {
+        pop_heap(begin(nums), end(nums), greater<int>());
+        ++nums.back();
+        push_heap(begin(nums), end(nums), greater<int>());        
     }
+    return accumulate(begin(nums), end(nums), 1LL, [](long long res, int n) { return res * n % 1000000007; });
+}
 };
