0d05f6dfd1e54339c35e6f6ac7b4154deec28f41
AKSHAT jain <73535628+akshat2580@users.noreply.github.com>
2022-08-15 12:54:30 +0530

    Time: 19 ms (50.83%), Space: 7.8 MB (43.03%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,77 +1,28 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        int n=s.length();int l=0;
-     if(n==1)
-     {  switch (s[0])
-          {
-              case 'M':
-              l=l+1000;
-              break;
-              case 'D':
-              l=l+500;
-              break;
-              case 'C':
-              {
-               l=l+100;break;} 
-              case 'L':
-l=l+50;
-              break;
-              
-              case 'V':
-              l=l+5;
-              break;
-              case 'X':
-              {
-                 
-              l=l+10;
-              break;}
-                   case 'I':
-              {
-                  l=l+1;
-              break;
-              }}}
-                      else{        
-                              for(int i=0;i<n;i++){
-          char ch=s[i];
-          
-          switch (ch)
-          {
-              case 'M':
-              l=l+1000;
-              break;
-              case 'D':
-              l=l+500;
-              break;
-              case 'C':
-              {if(s[i+1]=='M'||s[i+1]=='D')
-              l=l-100;
-              else
-               l=l+100;break;} 
-              case 'L':
-l=l+50;
-              break;
-              
-              case 'V':
-              l=l+5;
-              break;
-              case 'X':
-              {
-                      
-                  if(s[i+1]=='C'||s[i+1]=='L')
-              l=l-10;
-               else
-              l=l+10;
-              break;}
-                   case 'I':
-              {if(s[i+1]=='V'||s[i+1]=='X')
-                  l=l-1;
-              else
-                  l=l+1;
-              break;
-              }
-          }}
-            
-    }        return l;}
-
+   int romanToInt(string s) 
+{
+    unordered_map<char, int> T = { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+                                   
+   int sum = T[s.back()];
+   for (int i = s.length() - 2; i >= 0; --i) 
+   {
+       if (T[s[i]] < T[s[i + 1]])
+       {
+           sum -= T[s[i]];
+       }
+       else
+       {
+           sum += T[s[i]];
+       }
+   }
+   
+   return sum;
+}
 };
